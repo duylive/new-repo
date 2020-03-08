7c7ad01bc7ee79dea7082191ea2bde5b01981a3a
duylive <duylive92@gmail.com>
2020-03-07 17:52:13 -0800

    new

---
+++ abc.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Enter the width");
+inputHeight = prompt("Enter the height");
+let Width = parseInt(inputWidth);
+let Height = parseInt(inputHeight);
+let Area = Width * Height;
+document.write("The area is: " + Area);
